b3a104f19ec7e53da0151faf33667f964f818410
Mohan Chantada <mohanchantada@Mohans-MacBook-Air.local>
2025-08-08 14:42:31 +0100

    refactor key press test

--- tests/keyPressTest.spec.ts
+++ tests/keyPressTest.spec.ts
@@ -2,10 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test("Open home page and verify title", async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/');
-    // await expect(page.getByRole('heading', { name: 'Welcome to the-internet' })).toBeVisible();
-    // await expect(page.getByRole('heading', { name: 'Available Examples' })).toBeVisible();
-
-    //   await page.goto('https://the-internet.herokuapp.com/');
     await page.getByRole('link', { name: 'Key Presses' }).click();
     await page.getByRole('heading', { name: 'Key Presses' }).click();
     await page.getByText('Key presses are often used to').click();
